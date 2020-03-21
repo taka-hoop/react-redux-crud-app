df8cbb083c879eb8923b1ed894fa42a207220b6d
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-03-21 19:03:29 +0900

    file delete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>とりあえずreact立ち上げれた</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return <div><h1>hello world</h1></div>;
+  }
 }
 
 export default App;
