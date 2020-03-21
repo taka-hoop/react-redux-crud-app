fec7a0696edbe87b1770cfdb7335ed0048909088
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-03-21 17:37:04 +0900

    change p

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>とりあえずreact立ち上げれた</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
