5a794aadb2b0fd21ed3b2f9b4bd7ac24fdc66978
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-04-08 00:13:02 +0900

    修正

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redu'
-import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
-import reducer from './reducers'
+import reducer from './reducers';
 import App from './components/App';
-import App from './App';
-import registerSrviceWorker from './registerServiceWorker';
+
 
 const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+
 
 
