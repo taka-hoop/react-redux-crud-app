cd9b4360968d979821cd9a603a56d4425fdf5b06
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-04-07 18:41:51 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component} from 'react';
 
-class App extends Component {
-  render() {
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("push click")}} />;
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("push click")}} />;
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
+}
+const Cat = () => {
+  return <div>にゃ〜ん</div>
 }
 
 export default App;
